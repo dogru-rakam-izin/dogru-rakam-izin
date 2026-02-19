e271cb0607baeac9b779eb151eca83298b495e56
dogru-rakam-izin <dogrurakam@gmail.com>
2026-02-19 11:10:29 +0300

    Update ana_ekran.py

--- ana_ekran.py
+++ ana_ekran.py
@@ -10,8 +10,7 @@ st.set_page_config(page_title="Doğru Rakam İzin", layout="wide", page_icon=LOG
 
 # --- PERSONEL VERİTABANI ---
 PERSONEL_GIRISLERI = {
-    "ARİF EMRE YILDIZ": "2024-10-09","ARZU ÖZELMİŞ": "2024-10-09", 
-    "AYŞE KOLBAŞ": "2022-03-04",
+    "ARİF EMRE YILDIZ": "2024-10-09", "AYŞE KOLBAŞ": "2022-03-04",
     "AYŞE GÜLLÜ ÇIRAY": "2023-04-27", "BURAK ÖZAYDIN": "2025-09-11",
     "BUSE MEYRİLİ": "2025-02-07", "ERSİN KALSEN": "2023-06-06",
     "FERİDE CIKKAN": "2025-03-13", "GÖKÇE DÖNMEZKOL": "2025-06-24",
@@ -19,7 +18,8 @@ PERSONEL_GIRISLERI = {
     "İBRAHİM SOYLU": "2020-09-17", "MERVE ANAYURT": "2025-03-11",
     "MİZGİN BİDER": "2025-09-15", "NEFİSE NUR HOŞGÖR": "2025-06-09",
     "ÖZLEM KAPLAN": "2024-08-01", "PINAR TANRIVERDİ": "1900-01-01",
-    "SAMET DEMİREL": "2024-02-27", "SELEN ŞEN": "2025-11-03"
+    "SAMET DEMİREL": "2024-02-27", "ARZU ÖZELMİŞ": "2024-10-09",
+    "SELEN ŞEN": "2025-11-03"
 }
 
 # --- AYARLAR ---
